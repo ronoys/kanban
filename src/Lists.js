3394954b526a0478d37176afc812af4060b3ce06
ronoy <ronoysarkar.mail@gmail.com>
2019-09-20 22:18:34 -0400

    .

---
+++ src/Lists.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Lists extends Component {
+
+  
+  render() {
+    return (
+        <div>
+          <h1>Hello</h1>
+        </div>
+      );
+
+  }  
+}  
+
+export default Lists;
