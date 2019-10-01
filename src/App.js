013a49d4578c34e9b8d77e54f95494ee39fabf6a
ronoy <ronoysarkar.mail@gmail.com>
2019-09-30 20:16:23 -0400

    render new components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Lists from './Lists.js'
-import './App.css';
+import Second from './second.js'
 
 class App extends Component {
   render() {
     return(
-    <div className="App">
-      <Lists></Lists>
-      <p>Hello there</p>
+    <div className="App" >
+      
+      <Second></Second>
+      
+      
     </div>
     )
   };
